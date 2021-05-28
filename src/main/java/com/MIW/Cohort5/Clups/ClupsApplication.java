e4af497b7b457569155a77103e19dd06d2fcabc4
Steven Reijntjes <s.k.c.reijntjes@gmail.com>
2021-05-28 10:17:42 +0200

    Initialisation Spring project.

---
+++ src/main/java/com/MIW/Cohort5/Clups/ClupsApplication.java
@@ -0,0 +1,13 @@
+package com.MIW.Cohort5.Clups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MIW/Cohort5/Clups/ClupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MIW.Cohort5.Clups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
