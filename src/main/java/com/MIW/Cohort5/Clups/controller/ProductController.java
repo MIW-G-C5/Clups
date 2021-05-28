0ee843b9047106b6f50471569ee58586cb99331e
Johnnie Meijer <j.j.meijer@st.hanze.nl>
2021-05-28 11:30:08 +0200

    Created productOverview page, productRepository and productController

---
+++ src/main/java/com/MIW/Cohort5/Clups/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.MIW.Cohort5.Clups.controller;
+
+import com.MIW.Cohort5.Clups.model.Product;
+import com.MIW.Cohort5.Clups.repository.ProductRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Johnnie Meijer
+ * j.j.meijer@st.hanze.nl
+ */
+
+@Controller
+public class ProductController {
+
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping({"/", "/products"})
+    protected String showProducts(Model model) {
+        model.addAttribute("allProducts", productRepository.findAll());
+        return "productOverview";
+    }
+
+}
---
+++ src/main/java/com/MIW/Cohort5/Clups/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.MIW.Cohort5.Clups.repository;
+
+import com.MIW.Cohort5.Clups.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
