43305a4668908aa295e83c147acba56c6c641569
Johnnie Meijer <j.j.meijer@st.hanze.nl>
2021-06-16 17:02:52 +0200

    CL-26 CL-42 WIP All Customer functionality is now merged into User classes. Front-end pages still need to be merged

--- src/main/java/com/MIW/Cohort5/Clups/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.MIW.Cohort5.Clups.repository;
-
-import com.MIW.Cohort5.Clups.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
