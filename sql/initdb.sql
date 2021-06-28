4b435f39b875296a982939b459b0669e9e577c2f
Steven Reijntjes <s.k.c.reijntjes@gmail.com>
2021-06-28 11:53:56 +0200

    CL-38 Had to change the password in the initdb sql script

--- sql/initdb.sql
+++ sql/initdb.sql
@@ -2,5 +2,5 @@ DROP SCHEMA IF EXISTS clups;
 DROP USER IF EXISTS 'clupsDevUser'@'%';
 
 CREATE SCHEMA clups; -- Creates the new database
-CREATE USER 'clupsDevUser'@'%' identified by 'ClupsDevUserPassword'; -- Creates new database
+CREATE USER 'clupsDevUser'@'%' identified by 'pwclups'; -- Creates new database
 GRANT all ON clups.* to 'clupsDevUser'@'%'; -- Gives all priviliges to the new user on the newly created database
