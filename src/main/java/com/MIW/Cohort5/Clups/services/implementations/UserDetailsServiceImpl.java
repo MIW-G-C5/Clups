143de634ea73f2075ced7051ab791d6cc10bd9eb
Johnnie Meijer <j.j.meijer@st.hanze.nl>
2021-06-30 10:22:28 +0200

    CL-103 Fixed bug that editing your own name gave an IllegalArgumentException. You now keep your existing role without problems.

--- src/main/java/com/MIW/Cohort5/Clups/services/implementations/UserDetailsServiceImpl.java
+++ src/main/java/com/MIW/Cohort5/Clups/services/implementations/UserDetailsServiceImpl.java
@@ -106,11 +106,7 @@ public class UserDetailsServiceImpl implements UserService {
 
     private UserDto editRole(UserDto userDto) {
 
-        if (loggedInUser(userDto)) {
-            throw new IllegalArgumentException("You are not allowed to change your own role");
-        }
-
-        if (userDto.getUserRole() == null) {
+        if (userDto.getUserRole() == null || loggedInUser(userDto)) {
             userDto.setUserRole(userRepository.findUserByUserCode(userDto.getUserCode()).getRole().getRoleName());
         }
 
